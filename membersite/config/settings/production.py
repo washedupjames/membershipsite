010fb294db1497b05d48a5dd7d72065ca6895950
James Parker <parkerman23@gmail.com>
2023-11-05 22:24:20 +0000

    tester

--- membersite/config/settings/production.py
+++ membersite/config/settings/production.py
@@ -85,11 +85,10 @@ INSTALLED_APPS += ["storages"]  # noqa F405
 # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
 # AWS_S3_OBJECT_PARAMETERS = {
 #    "CacheControl": f"max-age={_AWS_EXPIRY}, s-maxage={_AWS_EXPIRY}, must-revalidate"
-}
 # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
 # AWS_S3_MAX_MEMORY_SIZE = env.int(
-    "DJANGO_AWS_S3_MAX_MEMORY_SIZE",
-    default=100_000_000,  # 100MB
+ #   "DJANGO_AWS_S3_MAX_MEMORY_SIZE",
+#    default=100_000_000,  # 100MB
 )
 # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
 # AWS_S3_REGION_NAME = env("DJANGO_AWS_S3_REGION_NAME", default=None)
