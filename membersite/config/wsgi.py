0fe52e4cb5e456524d7826cc4a4f61d493db35cb
James Parker <parkerman23@gmail.com>
2023-11-19 23:21:04 +0000

    trynew

--- membersite/config/wsgi.py
+++ membersite/config/wsgi.py
@@ -22,13 +22,14 @@ from django.core.wsgi import get_wsgi_application
 # This allows easy placement of apps within the interior
 # membersite directory.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
-sys.path.append(str(BASE_DIR / "membersite"))
+sys.path.insert(0, str(BASE_DIR / "membersite"))
+
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
 # os.environ["DJANGO_SETTINGS_MODULE"] = "config.settings.production"
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "membersite.config.settings.production")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
