80967fdf30fc5fc813aaa393462fb234bc1462be
James Parker <parkerman23@gmail.com>
2023-12-13 16:43:28 +0000

    mew

--- membersite/config/settings/base.py
+++ membersite/config/settings/base.py
@@ -166,54 +166,54 @@ LOCALE_PATHS = [str(BASE_DIR / "locale")]
 #     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 # ]
 
-# # STATIC
-# # ------------------------------------------------------------------------------
-# # https://docs.djangoproject.com/en/dev/ref/settings/#static-root
-# STATIC_ROOT = str(BASE_DIR / "staticfiles")
-# # https://docs.djangoproject.com/en/dev/ref/settings/#static-url
-# STATIC_URL = "/static/"
-# # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
-# STATICFILES_DIRS = [str(APPS_DIR / "static")]
-# # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
-# STATICFILES_FINDERS = [
-#     "django.contrib.staticfiles.finders.FileSystemFinder",
-#     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
-# ]
+# STATIC
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#static-root
+STATIC_ROOT = str(BASE_DIR / "staticfiles")
+# https://docs.djangoproject.com/en/dev/ref/settings/#static-url
+STATIC_URL = "/static/"
+# https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
+STATICFILES_DIRS = [str(APPS_DIR / "static")]
+# https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
 
-# # MEDIA
-# # ------------------------------------------------------------------------------
-# # https://docs.djangoproject.com/en/dev/ref/settings/#media-root
-# MEDIA_ROOT = str(APPS_DIR / "media")
-# # https://docs.djangoproject.com/en/dev/ref/settings/#media-url
-# MEDIA_URL = "/media/"
+# MEDIA
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#media-root
+MEDIA_ROOT = str(APPS_DIR / "media")
+# https://docs.djangoproject.com/en/dev/ref/settings/#media-url
+MEDIA_URL = "/media/"
 
-# # TEMPLATES
-# # ------------------------------------------------------------------------------
-# # https://docs.djangoproject.com/en/dev/ref/settings/#templates
-# TEMPLATES = [
-#     {
-#         # https://docs.djangoproject.com/en/dev/ref/settings/#std:setting-TEMPLATES-BACKEND
-#         "BACKEND": "django.template.backends.django.DjangoTemplates",
-#         # https://docs.djangoproject.com/en/dev/ref/settings/#dirs
-#         "DIRS": [str(APPS_DIR / "templates")],
-#         # https://docs.djangoproject.com/en/dev/ref/settings/#app-dirs
-#         "APP_DIRS": True,
-#         "OPTIONS": {
-#             # https://docs.djangoproject.com/en/dev/ref/settings/#template-context-processors
-#             "context_processors": [
-#                 "django.template.context_processors.debug",
-#                 "django.template.context_processors.request",
-#                 "django.contrib.auth.context_processors.auth",
-#                 "django.template.context_processors.i18n",
-#                 "django.template.context_processors.media",
-#                 "django.template.context_processors.static",
-#                 "django.template.context_processors.tz",
-#                 "django.contrib.messages.context_processors.messages",
-#                 "membersite.users.context_processors.allauth_settings",
-#             ],
-#         },
-#     }
-# ]
+# TEMPLATES
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#templates
+TEMPLATES = [
+    {
+        # https://docs.djangoproject.com/en/dev/ref/settings/#std:setting-TEMPLATES-BACKEND
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        # https://docs.djangoproject.com/en/dev/ref/settings/#dirs
+        "DIRS": [str(APPS_DIR / "templates")],
+        # https://docs.djangoproject.com/en/dev/ref/settings/#app-dirs
+        "APP_DIRS": True,
+        "OPTIONS": {
+            # https://docs.djangoproject.com/en/dev/ref/settings/#template-context-processors
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.template.context_processors.i18n",
+                "django.template.context_processors.media",
+                "django.template.context_processors.static",
+                "django.template.context_processors.tz",
+                "django.contrib.messages.context_processors.messages",
+                "membersite.users.context_processors.allauth_settings",
+            ],
+        },
+    }
+]
 
 # # https://docs.djangoproject.com/en/dev/ref/settings/#form-renderer
 # FORM_RENDERER = "django.forms.renderers.TemplatesSetting"
