386202d0fb2c31ffc38fc0febcaef2a697ead6db
James Parker <parkerman23@gmail.com>
2023-12-13 16:24:11 +0000

    mew

--- membersite/config/settings/local.py
+++ membersite/config/settings/local.py
@@ -2,81 +2,81 @@ from .base import *  # noqa
 from .base import env
 import os
 
-# GENERAL
-# ------------------------------------------------------------------------------
-# https://docs.djangoproject.com/en/dev/ref/settings/#debug
-DEBUG = True
-# https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
-SECRET_KEY = env(
-    "DJANGO_SECRET_KEY",
-    default="9ZPy5TndeKf8tInxQPEmaQ0qqLlVr4BgdI2ePvrlkkSct7dS7pYhLmdGwSdVCpDc",
-)
-# https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "f81f-82-132-224-41.ngrok-free.app",]
+# # GENERAL
+# # ------------------------------------------------------------------------------
+# # https://docs.djangoproject.com/en/dev/ref/settings/#debug
+# DEBUG = True
+# # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
+# SECRET_KEY = env(
+#     "DJANGO_SECRET_KEY",
+#     default="9ZPy5TndeKf8tInxQPEmaQ0qqLlVr4BgdI2ePvrlkkSct7dS7pYhLmdGwSdVCpDc",
+# )
+# # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
+# ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "f81f-82-132-224-41.ngrok-free.app",]
 
-# CACHES
-# ------------------------------------------------------------------------------
-# https://docs.djangoproject.com/en/dev/ref/settings/#caches
-CACHES = {
-    "default": {
-        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
-        "LOCATION": "",
-    }
-}
+# # CACHES
+# # ------------------------------------------------------------------------------
+# # https://docs.djangoproject.com/en/dev/ref/settings/#caches
+# CACHES = {
+#     "default": {
+#         "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+#         "LOCATION": "",
+#     }
+# }
 
-# EMAIL
-# ------------------------------------------------------------------------------
-# https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
-EMAIL_BACKEND = env(
-    "DJANGO_EMAIL_BACKEND", default="django.core.mail.backends.console.EmailBackend"
-)
+# # EMAIL
+# # ------------------------------------------------------------------------------
+# # https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
+# EMAIL_BACKEND = env(
+#     "DJANGO_EMAIL_BACKEND", default="django.core.mail.backends.console.EmailBackend"
+# )
 
-# django-debug-toolbar
-# ------------------------------------------------------------------------------
-# https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#prerequisites
-# INSTALLED_APPS += ["debug_toolbar"]  # noqa F405
-# https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#middleware
-# MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]  # noqa F405
-# https://django-debug-toolbar.readthedocs.io/en/latest/configuration.html#debug-toolbar-config
-# DEBUG_TOOLBAR_CONFIG = {
-#   "DISABLE_PANELS": ["debug_toolbar.panels.redirects.RedirectsPanel"],
-#  "SHOW_TEMPLATE_CONTEXT": True,}
+# # django-debug-toolbar
+# # ------------------------------------------------------------------------------
+# # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#prerequisites
+# # INSTALLED_APPS += ["debug_toolbar"]  # noqa F405
+# # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#middleware
+# # MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]  # noqa F405
+# # https://django-debug-toolbar.readthedocs.io/en/latest/configuration.html#debug-toolbar-config
+# # DEBUG_TOOLBAR_CONFIG = {
+# #   "DISABLE_PANELS": ["debug_toolbar.panels.redirects.RedirectsPanel"],
+# #  "SHOW_TEMPLATE_CONTEXT": True,}
 
-# https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#internal-ips
-INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
-if env("USE_DOCKER") == "yes":
-    import socket
+# # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#internal-ips
+# INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
+# if env("USE_DOCKER") == "yes":
+#     import socket
 
-    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
-    INTERNAL_IPS += [".".join(ip.split(".")[:-1] + ["1"]) for ip in ips]
+#     hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+#     INTERNAL_IPS += [".".join(ip.split(".")[:-1] + ["1"]) for ip in ips]
 
-# django-extensions
-# ------------------------------------------------------------------------------
-# https://django-extensions.readthedocs.io/en/latest/installation_instructions.html#configuration
-INSTALLED_APPS += ["django_extensions"]  # noqa F405
-# Celery
-# ------------------------------------------------------------------------------
+# # django-extensions
+# # ------------------------------------------------------------------------------
+# # https://django-extensions.readthedocs.io/en/latest/installation_instructions.html#configuration
+# INSTALLED_APPS += ["django_extensions"]  # noqa F405
+# # Celery
+# # ------------------------------------------------------------------------------
 
-# https://docs.celeryq.dev/en/stable/userguide/configuration.html#task-eager-propagates
-# CELERY_TASK_EAGER_PROPAGATES = True
-# Your stuff...
-# ------------------------------------------------------------------------------
+# # https://docs.celeryq.dev/en/stable/userguide/configuration.html#task-eager-propagates
+# # CELERY_TASK_EAGER_PROPAGATES = True
+# # Your stuff...
+# # ------------------------------------------------------------------------------
 
-# Stripe
-STRIPE_PUBLIC_KEY = env("STRIPE_PUBLIC_KEY")
-STRIPE_SECRET_KEY = env("STRIPE_SECRET_KEY")
+# # Stripe
+# STRIPE_PUBLIC_KEY = env("STRIPE_PUBLIC_KEY")
+# STRIPE_SECRET_KEY = env("STRIPE_SECRET_KEY")
 
-CSRF_TRUSTED_ORIGINS = ['https://f81f-82-132-224-41.ngrok-free.app', ]
-
-
+# CSRF_TRUSTED_ORIGINS = ['https://f81f-82-132-224-41.ngrok-free.app', ]
 
 
-# Email Settings
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = os.environ.get('GMAIL_ACCOUNT')
-EMAIL_HOST_PASSWORD = os.environ.get('GMAIL_APP_PASSWORD')
-EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = os.environ.get('GMAIL_ACCOUNT')
+
+
+# # Email Settings
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = os.environ.get('GMAIL_ACCOUNT')
+# EMAIL_HOST_PASSWORD = os.environ.get('GMAIL_APP_PASSWORD')
+# EMAIL_USE_TLS = True
+# DEFAULT_FROM_EMAIL = os.environ.get('GMAIL_ACCOUNT')
 
